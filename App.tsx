ccba14614d9c1b09a7e7c97b9ce7110f829a89b9
ricardo <carrilloricardo588@gmail.com>
2023-03-31 05:05:46 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar, 
+  TextInput, 
+  Button,
+  TouchableOpacity
+} from 'react-native';
+ 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App (){
+  return(
+    <>
+      <View>
+        <View>
+          <Text></Text>
+        </View>
+        <View>
+
+        </View>
+      </View>
+    </>
+  )
+}
+export default App
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import DataInputScreen from './Lista de deseos/screens/DataInputScreen';
+import BudgetScreen from './src/screens/BudgetScreen';
+
+AppRegistry.registerComponent(appName, () => DataInputScreen);
